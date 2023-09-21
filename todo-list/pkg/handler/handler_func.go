d54f7d5d084e3a72999ea7facbe4f7ec68085c0b
magavales <filippovdenis-2002@yandex.ru>
2023-09-21 16:01:00 +0300

    Add the handler and endpoint which delete task from table by id

--- todo-list/pkg/handler/handler_func.go
+++ todo-list/pkg/handler/handler_func.go
@@ -41,7 +41,7 @@ func (h *Handler) getTaskByID(ctx *gin.Context) {
 	}
 }
 
-func (h *Handler) updateTask(ctx *gin.Context) {
+func (h *Handler) updateTaskByID(ctx *gin.Context) {
 	var (
 		db   database.Database
 		task model.Task
@@ -57,6 +57,27 @@ func (h *Handler) updateTask(ctx *gin.Context) {
 
 	db.Connect()
 	err = db.Access.UpdateTask(db.Pool, task)
+	if err != nil {
+		return
+	}
+}
+
+func (h *Handler) deleteTaskByID(ctx *gin.Context) {
+	var (
+		db   database.Database
+		task model.Task
+		err  error
+	)
+	err = task.DecodeJSON(ctx.Request.Body)
+	if err != nil {
+		return
+	}
+
+	id, _ := strconv.Atoi(ctx.Param("id"))
+	task.ID = int64(id)
+
+	db.Connect()
+	err = db.Access.DeleteTask(db.Pool, task.ID)
 	if err != nil {
 		return
 	}
