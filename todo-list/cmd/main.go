1073d3ab0a2cc64a86b2ae2cc60062e5ccfc840f
magavales <filippovdenis-2002@yandex.ru>
2023-09-24 17:53:06 +0300

    Add configs

--- todo-list/cmd/main.go
+++ todo-list/cmd/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	"todo-list/pkg/handler"
 	"todo-list/pkg/server"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err)
+	}
 	router := new(handler.Handler).InitRouter()
 
 	serv := new(server.Server)
-	err := serv.InitServer("8080", router)
+	err := serv.InitServer(viper.GetString("port"), router)
 	if err != nil {
 		log.Fatalf("Server can't be opened: %s", err)
 	}
+}
+func initConfig() error {
+	viper.SetConfigName("config_server")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath("configs")
+
+	return viper.ReadInConfig()
 }
--- todo-list/pkg/database/database.go
+++ todo-list/pkg/database/database.go
@@ -2,7 +2,10 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/spf13/viper"
+	"log"
 	"todo-list/pkg/database/table"
 )
 
@@ -12,10 +15,21 @@ type Database struct {
 }
 
 func (db *Database) Connect() error {
-	poolConn, _ := pgxpool.ParseConfig("user=postgres password=1703 host=localhost port=5432 dbname=postgres pool_max_conns=10")
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err)
+	}
+	poolConn, _ := pgxpool.ParseConfig(fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s pool_max_conns=%s", viper.GetString("user"), viper.GetString("password"), viper.GetString("host"), viper.GetString("port"), viper.GetString("dbname"), viper.GetString("conns")))
 
 	var err error
 	db.Pool, err = pgxpool.NewWithConfig(context.Background(), poolConn)
 
 	return err
+}
+
+func initConfig() error {
+	viper.SetConfigName("config_database")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath("configs")
+
+	return viper.ReadInConfig()
 }
